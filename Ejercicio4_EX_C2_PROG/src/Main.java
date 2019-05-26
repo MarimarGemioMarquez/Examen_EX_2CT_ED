e34c5313728489af7e00529dc73b6fc3c5b23728
marim <marim@LAPTOP-0S0KNB7H.Home>
2019-05-26 09:40:56 +0200

    Cambios de inicio y fin de programa.

--- Ejercicio4_EX_C2_PROG/src/Main.java
+++ Ejercicio4_EX_C2_PROG/src/Main.java
@@ -3,12 +3,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		System.out.println("Inicio de programa");
 		// Creamos un nuevo directivo
 		Directivo directivo = new Directivo("Jose Luis Perales", 1000, 3);
 		// Imprimimos los datos del Empleado + la clase Directivos.
 		directivo.imprimirDatos();
 		// Mostramos los datos del sueldo como directivo
-		System.out.println("Sueldo como directivo : " + directivo.obtenerSalario());
+		System.out.println("Sueldos como directivo : " + directivo.obtenerSalario());
+		System.out.println("Fin de programa");
 
 	}
 
